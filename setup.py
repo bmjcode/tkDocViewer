eee02fe3c27a2058b8e7961624d74aa18b6b9893
Benjamin Johnson <42893476+bmjcode@users.noreply.github.com>
2019-09-08 14:40:49 -0400

    Bump the development status to Production/Stable

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("README.md", "r") as readme:
 URL = "https://github.com/bmjcode/tkDocViewer"
 PACKAGES = find_packages()
 CLASSIFIERS = [
-    "Development Status :: 4 - Beta",
+    "Development Status :: 5 - Production/Stable",
     "Programming Language :: Python :: 2",
     "Programming Language :: Python :: 3",
     "License :: OSI Approved :: MIT License",
